1bab9d5abbade58df0c78fd57025387f1b3cf9d1
EirikOsnes <eirik.osnes@gmail.com>
2020-02-19 15:46:56 +0100

    Set up Oculus headset, controllers and pointer visuals

---
+++ Assets/Scripts/LaserPointerHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointerHandler : MonoBehaviour
+{
+
+    public LaserPointer pointer;
+    public OVRInput.RawButton laserPointerButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointer.laserBeamBehavior = LaserPointer.LaserBeamBehavior.Off;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(laserPointerButton))
+        {
+            pointer.laserBeamBehavior = LaserPointer.LaserBeamBehavior.On;
+        } else
+        {
+            pointer.laserBeamBehavior = LaserPointer.LaserBeamBehavior.Off;
+            pointer.cursorVisual.SetActive(false);
+        }
+    }
+}
